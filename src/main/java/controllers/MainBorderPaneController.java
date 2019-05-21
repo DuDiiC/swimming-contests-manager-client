cbcf9c0e24317c00b830875027e9f0f63eb45a0b
Maciej Dudek <maciejdudek96@wp.pl>
2019-05-21 18:53:09 +0200

    Add views to app

--- src/main/java/controllers/MainBorderPaneController.java
+++ src/main/java/controllers/MainBorderPaneController.java
@@ -9,6 +9,14 @@ import javafx.scene.layout.BorderPane;
 
 public class MainBorderPaneController {
 
+    private static final String CLUB_VIEW_FXML = "/fxml/ClubView.fxml";
+    private static final String CONTEST_VIEW_FXML = "/fxml/ContestView.fxml";
+    private static final String COMPETITOR_VIEW_FXML = "/fxml/CompetitorView.fxml";
+    private static final String TRAINER_VIEW_FXML = "/fxml/TrainerView.fxml";
+    private static final String COMPETITION_VIEW_FXML = "/fxml/CompetitionView.fxml";
+    private static final String RECORD_VIEW_FXML = "/fxml/RecordView.fxml";
+    private static final String REGISTRATION_VIEW_FXML = "/fxml/RegistrationView.fxml";
+
     @FXML private BorderPane borderPane;
 
     @FXML private ToggleGroup tables;
@@ -32,28 +40,35 @@ public class MainBorderPaneController {
 
     @FXML
     public void viewCompetitions() {
+        setCenter(COMPETITION_VIEW_FXML);
     }
 
     @FXML
     public void viewCompetitors() {
+        setCenter(COMPETITOR_VIEW_FXML);
     }
 
     @FXML
     public void viewRecords() {
+        setCenter(RECORD_VIEW_FXML);
     }
 
     @FXML
     public void viewClubs() {
+        setCenter(CLUB_VIEW_FXML);
     }
 
     @FXML
     public void viewTrainers() {
+        setCenter(TRAINER_VIEW_FXML);
     }
 
     @FXML
     public void viewRegistration() {
+        setCenter(REGISTRATION_VIEW_FXML);
     }
 
     private void setCenter(String fxmlPath) {
+        borderPane.setCenter(FxmlUtil.fxmlLoader(fxmlPath));
     }
 }
