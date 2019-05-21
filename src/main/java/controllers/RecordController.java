8e28e9abf17386ab308698de4d94b764f7d8de11
Maciej Dudek <maciejdudek96@wp.pl>
2019-05-21 18:54:44 +0200

    Add recordController

---
+++ src/main/java/controllers/RecordController.java
@@ -0,0 +1,26 @@
+package controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
+
+public class RecordController {
+
+    @FXML
+    private ComboBox competitorComboBox;
+
+    @FXML
+    private ComboBox competitionComboBox;
+
+    @FXML
+    private TextField timeTextField;
+
+    @FXML
+    private Button addRecordButton;
+
+    @FXML
+    public void addRecord() {
+
+    }
+}
