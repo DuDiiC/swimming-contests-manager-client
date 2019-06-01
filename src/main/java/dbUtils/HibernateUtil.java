929fb62347015fd4d07d094936fe21f3ab21c741
Maciej Dudek <maciejdudek96@wp.pl>
2019-06-01 23:42:24 +0200

    Add login panel

--- src/main/java/dbUtils/HibernateUtil.java
+++ src/main/java/dbUtils/HibernateUtil.java
@@ -5,21 +5,33 @@ import org.hibernate.cfg.Configuration;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+import javax.persistence.PersistenceException;
+import java.util.Properties;
 
 public class HibernateUtil {
 
     private static final String HIBERNATE_CFG_XML = "META-INF/hibernate.cfg.xml";
 
-    private static EntityManager em = createEM();
+    private static EntityManager em/* = createEM()*/;
 
-    public static EntityManager createEM() {
+    public static EntityManager createEM(String user, String passwd) throws PersistenceException {
+
         Configuration configuration = new Configuration().configure(HIBERNATE_CFG_XML);
-        EntityManagerFactory emf = Persistence.createEntityManagerFactory("persistence1");
+        configuration.getProperties().setProperty("connection.username", user);
+        configuration.getProperties().setProperty("connection.password", passwd);
+        Properties props = new Properties();
+        props.setProperty("javax.persistence.jdbc.user", user);
+        props.setProperty("javax.persistence.jdbc.password", passwd);
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("persistence1", props);
         return emf.createEntityManager();
     }
 
     public static EntityManager getEm() {
         return em;
+    }
+
+    public static void setEm(EntityManager em) {
+        HibernateUtil.em = em;
     }
 }
 
--- src/main/java/fxUtils/DialogsUtil.java
+++ src/main/java/fxUtils/DialogsUtil.java
@@ -13,4 +13,13 @@ public class DialogsUtil {
         alert.setContentText(alertText);
         alert.showAndWait();
     }
+
+    public static void informationDialog(String alertText) {
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.initStyle(StageStyle.UNDECORATED);
+        alert.setTitle("Informacja");
+        alert.setHeaderText("Informacja");
+        alert.setContentText(alertText);
+        alert.showAndWait();
+    }
 }
