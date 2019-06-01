d47fbccc30df721f7a408de71eebb405314d82f4
Maciej Dudek <maciejdudek96@wp.pl>
2019-06-01 23:41:17 +0200

    Update record hibernate util

--- src/main/java/dbUtils/HibernateUtilRecord.java
+++ src/main/java/dbUtils/HibernateUtilRecord.java
@@ -1,17 +1,28 @@
 package dbUtils;
 
+import dbModels.Competition;
+import dbModels.Competitor;
 import dbModels.Record;
+import fxUtils.DialogsUtil;
 
 import javax.persistence.EntityManager;
+import java.util.List;
 
 public class HibernateUtilRecord {
 
     private static EntityManager em = HibernateUtil.getEm();
 
-    public static void addRecord(Record record) {
+    public static boolean addRecord(Record record) {
+        List<Record> rList = HibernateUtilCompetitor.getAllRecords(record.getCompetitor());
+        for(Record r : rList) {
+            if(r.getCompetition() == record.getCompetition() && r.compareTo(record) < 0) {
+                return false;
+            }
+        }
         em.getTransaction().begin();
         em.persist(record);
         em.getTransaction().commit();
+        return true;
     }
 
     public static void removeRecord(Record record) {
